8f03e09be469ef69a7bbbe3555364ca10cb08508
Navin S <navinzoro7@gmail.com>
2025-12-12 12:29:25 +0530

    Initialized with Schema for admin and Assignments

--- db-connect/db.js
+++ db-connect/db.js
@@ -1,10 +1,13 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
+
 exports.connect = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/sri-eshwar-backend-1");
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("Db is connected");
   } catch (e) {
     console.log("Error in connecting to the db");
+    console.log(e);
   }
 };
 
